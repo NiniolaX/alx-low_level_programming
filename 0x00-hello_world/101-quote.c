de70b9f6229fbcd0ce45211e04da3d53a6462fab
NiniolaX <loveafinni16@gmail.com>
2023-04-23 06:42:17 -0700

    Solution to Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * Description: main - Prints string to standard output.
+ * Return: Returns 0 if success.
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",(unsigned long)sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
+}
