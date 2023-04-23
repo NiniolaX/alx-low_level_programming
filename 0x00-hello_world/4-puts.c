b4c790df8ba07ad00c5510005b741e7fe487b81c
NiniolaX <loveafinni16@gmail.com>
2023-04-23 02:03:28 -0700

    Correction3 - Solution to Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 
 /*
- * main - Prints a defined string 
- *
- * char str[] - defines string to be outputted
- *
- * puts(str) - prints string
- *
+ * main - Writes a string to standard output. 
  * return - returns '0' if success
  */
 
-int main (void)
+int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-
-	puts(str);
-
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
