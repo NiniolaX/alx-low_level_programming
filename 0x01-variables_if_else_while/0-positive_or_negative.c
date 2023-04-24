e32f27c30d06a90799a2badb36adb577e177da02
NiniolaX <loveafinni16@gmail.com>
2023-04-24 15:41:01 -0700

    Correction1 - Solution to Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,13 @@ int main(void)
 	printf("%d", n);
 	if (n > 0)
 	{
-		printf(" is positive");
+		printf(" is positive\n");
 	} else if (n == 0)
 	{
-		printf(" is zero");
+		printf(" is zero\n");
 	} else
 	{
-		printf(" is negative");
+		printf(" is negative\n");
 	}
 	return (0);
 }
