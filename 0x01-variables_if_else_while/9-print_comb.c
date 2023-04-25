88427d3a389d0103845aaff12db9994429aa5798
NiniolaX <loveafinni16@gmail.com>
2023-04-24 19:57:40 -0700

    Solution to Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,18 +5,17 @@
  */
 
 int main(void) {
-    int i, j, k;
+	int num;
 
-    for(i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++) {
-            for(k = 0; k < 10; k++) {
-                putchar(i + '0');
-                putchar(j + '0');
-                putchar(k + '0');
-		putchar(',');
-            }
-        }
-    }
-    return (0);
+	for (num = 0; num < 10 ; num++)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
