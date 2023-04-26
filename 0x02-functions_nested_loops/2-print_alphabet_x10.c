57ef8aefc735bf6ea1560a7ca7fa54ee5ead3f7b
NiniolaX <loveafinni16@gmail.com>
2023-04-25 22:16:01 -0700

    Solution to Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * Description: main - Prints alphabet in lowercase 10 times.
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+	i = 0;
+
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
