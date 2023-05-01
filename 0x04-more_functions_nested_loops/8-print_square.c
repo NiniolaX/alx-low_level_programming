648096b7fd89e0ac2d371b1fca08be60c47b0cdc
NiniolaX <loveafinni16@gmail.com>
2023-05-01 13:36:12 -0700

    Function to print a square using putchar

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * Description: print_square - Prints a square.
+ * @size: Size of square to be printed.
+ * Return: Void.
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+}
