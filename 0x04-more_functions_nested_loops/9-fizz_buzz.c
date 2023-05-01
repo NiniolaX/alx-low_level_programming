2a1b197d22c2d4a288afb8d832df0bb15ba30be3
NiniolaX <loveafinni16@gmail.com>
2023-05-01 14:01:21 -0700

    The Fizz-Buzz Test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * Description: main - The Fizz-Buzz test.
+ * Return: 0 if success.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
