e4497f1d702c2c21657b44466876b1384959dafa
NiniolaX <loveafinni16@gmail.com>
2023-05-03 08:46:45 -0700

    Function prints a string elements in index increments of 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * Description: puts2 - Prints string characters in increments by 2.
+ * @str: String to be printed.
+ * Return: Void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int length = 0;
+
+	/* Calculates length of string */
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	/* Print string characters */
+	for (i = 0; i < length - 1; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
