2e43c851b02d9341a7de86b529f779493f124fdb
NiniolaX <loveafinni16@gmail.com>
2023-05-02 18:18:46 -0700

    Passing parameter by reference

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * Description: reset_to_98 - Updates the value it points to to 98.
+ * @n: Pointer parameter used for update.
+ * Return: Void.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
