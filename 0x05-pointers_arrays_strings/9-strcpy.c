dbf96b419bead37e3c246bd5001eda1e43620f80
NiniolaX <loveafinni16@gmail.com>
2023-05-03 17:11:33 -0700

    Function copies a string into another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * Description: _strcpy - Copies a string including null terminator.
+ * @dest: String element destination.
+ * @src: String element source.
+ * Return: Pointer to 'dest'.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	/* Calculate the length of the source string */
+	int length = 0;
+
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+
+	/* Copy characters in string src to dest */
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
