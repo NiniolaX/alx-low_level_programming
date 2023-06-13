c2d2eade4c61ec4b038a5487975ad5de69a0805f
NiniolaX <loveafinni16@gmail.com>
2023-06-13 16:37:53 -0700

    Added function to free allocated memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 /**
+ * free_array - Frees memory space allocated to array
+ * @array: address to be freed
+ * @row: number of pointers in address or number of rows in  array
+ * Return: void
+ */
+void free_array(int **array, int row)
+{
+	int i;
+	/* Free space allocated for elements in each row */
+	for (i = 0; i < row; i++)
+	{
+		free(array[i]);
+	}
+
+	/* Free space allocated for each row or bloc */
+	free(array);
+}
+/**
  * alloc_grid - Returns a pointer to a 2-dimensional array of integers
  * @width: Width of array (i.e. numer of columns)
  * @height: Height of array (i.e. number of rows)
@@ -10,7 +28,7 @@
 
 int **alloc_grid(int width, int height)
 {
-	int row = height, col = width, i, j, k, l;
+	int row = height, col = width, i, j;
 	int **array;
 
 	if (row <= 0 || col <= 0)
@@ -20,36 +38,28 @@ int **alloc_grid(int width, int height)
 
 	/* Allocate memory for row number of blocks/rows */
 	array = (int **)malloc(row * sizeof(int *));
-
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 	/* Allocate memory for col number of integers in each row */
 	for (i = 0; i < row; i++)
 	{
 		array[i] = (int *)malloc(col * sizeof(int));
 	}
-
-	if (array == NULL || *array == NULL)
+	if (*array == NULL)
 	{
-		for (i = 0; i < row; i++)
-		{
-			free(array[i]);
-		}
-		free(array);
+		free_array(array, row);
 		return (NULL);
 	}
-	for (j = 0; j < row; j++)
+	for (i = 0; i < row; i++)
 	{
-		for (k = 0; k < col; k++)
+		for (j = 0; j < col; j++)
 		{
-			array[j][k] = 0;
+			array[i][j] = 0;
 		}
 	}
 	return (array);
 
-	/* Free memory reserved for col no of elements in each row */
-	for (l = 0; l < row; l++)
-	{
-		free(array[l]);
-	}
-	/* Free memory reserved for each row */
-	free(array);
+	free_array(array, row);
 }
