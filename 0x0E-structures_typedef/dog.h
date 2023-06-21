f29b3794b127965b6f374f646a5a63fda6868cd2
NiniolaX <loveafinni16@gmail.com>
2023-06-20 20:45:13 -0700

    Added description for struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_HEADER
 #define DOG_HEADER
 
+/**
+ * struct dog - Defines a new type struct dog
+ * @name: Name of dog
+ * @owner: Name of owner
+ * @age: Age of dog
+ *
+ * Descripton: Defines a new type struct dog with elements name, owner and age.
+ */
+
 struct dog
 {
 	char *name;
