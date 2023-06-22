efa90313d9e26f5024ca09e57ddf2e9f68b46d3c
NiniolaX <loveafinni16@gmail.com>
2023-06-22 05:26:21 -0700

    Corrected comment to reflect variable been initialized is d not init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -19,7 +19,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		return;
 	}
 
-	/* Now that mem alloc is complete, initialize init_dog */
+	/* Now that mem alloc is complete, initialize variable d */
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
