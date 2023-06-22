302e11d9425c6bf857b8d04fae33ecf13322b2cf
NiniolaX <loveafinni16@gmail.com>
2023-06-22 05:14:40 -0700

    Prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,20 +9,22 @@
 
 void print_dog(struct dog *d)
 {
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
 	if (d->owner == NULL)
 	{
 		d->owner = "(nil)";
 	}
 
-	if (d->name == NULL)
+	/* Print nothing if d is NILL */
+	if (d != NULL)
 	{
-		d->name = "(nil)";
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
-
-	if (d == NULL)
+	else
 	{
 		return;
 	}
-
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
