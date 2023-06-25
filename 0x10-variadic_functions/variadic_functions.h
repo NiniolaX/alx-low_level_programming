2ead2a00c0bea564d624e0b8a0a8bddf7720272a
NiniolaX <loveafinni16@gmail.com>
2023-06-25 03:28:21 -0700

    Included standard library

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,9 @@
 #ifndef VARIADIC_FUNC_HEADER
 #define VARIADIC_FUNC_HEADER
 
+#include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
