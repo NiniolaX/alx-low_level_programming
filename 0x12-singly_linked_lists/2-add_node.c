7183ebf11b9fc970496b84d01a9cb7288801246d
NiniolaX <loveafinni16@gmail.com>
2023-07-04 08:19:28 -0700

    Modified code base by using most efficient algorithm

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,22 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - Calculates the length of a string
- * @s: Pointer to string
- * Return: Length of string
- */
-unsigned int _strlen(const char *s)
-{
-	unsigned int length = 0;
-
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-
-	return (length);
-}
-/**
  * add_node - Adds a new node at a beginning of a singly linked list list_t
  * @head: Head of linked list
  * @str: String to be added to beginning of node
@@ -26,6 +10,7 @@ unsigned int _strlen(const char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len = 0;
 
 	/* Allocate memory for new head node, return NULL if malloc fails */
 	new_node = malloc(sizeof(list_t));
@@ -34,9 +19,15 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
+	/* Calculate length of string argument passed */
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
 	/* Initialize elements of the new node */
 	new_node->str = strdup(str);
-	new_node->len = _strlen(str);
+	new_node->len = len;
 	new_node->next = *head;
 
 	/* Store address of new node in head */
