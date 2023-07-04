196d258057eae5444d5784e784faccf496ae444b
NiniolaX <loveafinni16@gmail.com>
2023-07-04 08:56:31 -0700

    Frees singly linked list list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list
+ * @head: Head node of list
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	/* Pointer temp to hold the address of next node temporarily */
+	list_t *temp = head;
+
+	while (head != NULL)
+	{
+		/* Store address of next node in temp & move pointer to node */
+		temp = head->next;
+		/* Free node head */
+		free(head);
+		/* Re-assign address of next node to head */
+		head = temp;
+	}
+}
