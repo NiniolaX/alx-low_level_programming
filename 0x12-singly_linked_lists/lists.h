c4642bf784c1fae7f87cd61ed33c7b08a68aa3cc
NiniolaX <loveafinni16@gmail.com>
2023-07-03 21:06:03 -0700

    Included strings.h library for strdup operations

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,9 @@
 #define LISTS_HEADER
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <strings.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string (malloc'ed string)
@@ -23,5 +26,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+unsigned int _strlen(const char *s);
 
 #endif
