b022841d84e2d0d456630afed34dacbe3c5fab8d
NiniolaX <loveafinni16@gmail.com>
2023-07-04 12:55:55 -0700

    Function prints a character string before main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
  * Return: void
  */
 
-void __attribute__((constructor)) before_main(void)
+void __attribute__((constructor)) str_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
