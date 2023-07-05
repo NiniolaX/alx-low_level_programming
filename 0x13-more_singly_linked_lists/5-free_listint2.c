ec85992c0b67c345a8a6f727cefc5a15e0e61c66
NiniolaX <loveafinni16@gmail.com>
2023-07-05 12:30:03 -0700

    Added condition for when NULL is passed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,6 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+	{
+		return;
+	}
+
 	while (*head != NULL)
 	{
 		/* Store address of next node in temp and transverse to it */
