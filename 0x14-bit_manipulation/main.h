7a9d8be5b03263fcf2b026e317c6ef92fe1bd9ae
NiniolaX <loveafinni16@gmail.com>
2023-07-07 09:59:12 -0700

    Included stddef header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef BIT_HEADER
 #define BIT_HEADER
 
+#include <stddef.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
