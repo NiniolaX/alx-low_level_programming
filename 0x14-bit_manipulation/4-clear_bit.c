f2748efcc8fb8b548ca03cca0df0ca7f084ab955
NiniolaX <loveafinni16@gmail.com>
2023-07-08 11:09:30 -0700

    Adjusted code to do nothing if bit at index is 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,20 +11,15 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned long int temp = *n;
 	unsigned int maxIndex = sizeof(n) * 8;
 
-	/* Checks non-NULL pointer and in-range index is passed */
+	/* Checks that valid address and in-range index is passed */
 	if (n == NULL || index > maxIndex)
 		return (-1);
 
-	/* Checks what bit is at index and uses the right operator to off it */
+	/* Checks if bit at index is 1 and turns it off */
 	if (((temp >> index) & 1) == 1)
 	{
-		/* Bit is 1 */
+		/* Bit at index is 1 */
 		*n = *n ^ (1 << index);
-	}
-	if (((temp >> index) & 1) == 0)
-	{
-		/* Bit is 0 */
-		*n = *n & (1 << index);
 	}
 
 	return (1);
