e253c2cca765707678b7db17be7c5cdd9e4fe6ff
NiniolaX <loveafinni16@gmail.com>
2023-07-08 11:24:25 -0700

    Improved readability of codebase

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -16,7 +16,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 
 	/* Sets bit at index to 1 and puts result back in n */
-	*n |= (1 << index);
+	*n = *n | (1 << index);
 
 	return (1);
 }
