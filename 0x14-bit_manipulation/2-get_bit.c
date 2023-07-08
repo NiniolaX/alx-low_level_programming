53c47a62e5799d5e80ae8371b2e4204b635a895f
NiniolaX <loveafinni16@gmail.com>
2023-07-08 08:02:30 -0700

    Added check for out of range index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,9 +8,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int maxIndex = sizeof(n) * 8;
+
+	/* Checks that index passed is not out of range */
+	if (index > maxIndex)
+		return (-1);
 	/**
-	 * Right shift number by index to move bit to index 0
-	 * Apply bitwise & to result from (1) to check what bit is at index 0
+	 * (1)Right shift number by index to move bit to index 0
+	 * (2)Apply bitwise & to result from(1) to check what bit is at index 0
 	 */
 	if (((n >> index) & 1) == 0)
 	{
