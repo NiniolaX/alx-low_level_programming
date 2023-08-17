73bd17d58b67f8f7a34d4cf9c30187104e73612a
NiniolaX <loveafinni16@gmail.com>
2023-08-17 10:45:07 -0700

    Removed condition to return NULL if head is NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,9 +11,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
