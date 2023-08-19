c3477d3523df191ab5d165fefd0097ae66176275
NiniolaX <loveafinni16@gmail.com>
2023-08-19 11:38:07 -0700

    Fixed segfault error when deleting node at idx 0

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -18,7 +18,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		*head = (*head)->next;
+		*head = ptr->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		free(ptr);
 		return (1);
 	}
