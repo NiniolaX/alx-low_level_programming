ec0844fd2f98c777e702f7ef7a45c696abd6d5cf
NiniolaX <loveafinni16@gmail.com>
2023-09-09 09:56:00 -0700

    Fixed syntax error - Invalid initializer for ptr */

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new = NULL, ptr = NULL;
+	hash_node_t *new = NULL, *ptr = NULL;
 
 	if (ht == NULL || key == NULL)
 		return (0);
