97e60762ef20b3f7ea165d9a27161e14f8213b32
NiniolaX <loveafinni16@gmail.com>
2023-09-09 09:41:06 -0700

    Wrote function that adds an element to the hash table

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,7 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index = -1;
+	unsigned long int index;
 
 	if (key != NULL && size != 0)
 		index = hash_djb2(key) % size;
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: Pointer to hash table
+ * @key: key
+ * @value: value associated with key
+ * Return: 1 if success, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new = NULL;
+
+	if (ht == NULL || key == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	/* No collision */
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = malloc(sizeof(hash_node_t));
+		ht->array[index]->key = (char *)key;
+		if (value != NULL)
+			ht->array[index]->value = strdup(value);
+		else
+			ht->array[index]->value = NULL;
+		ht->array[index]->next = NULL;
+	}
+	/* Collision */
+	else
+	{
+		new = malloc(sizeof(hash_node_t));
+		new->key = (char *)key;
+		if (value != NULL)
+			new->value = strdup(value);
+		else
+			new->value = NULL;
+		new->next = NULL;
+		ht->array[index]->next = new;
+	}
+
+	return (1);
+}
