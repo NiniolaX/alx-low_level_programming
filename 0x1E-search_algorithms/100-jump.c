1748e111e9980b1aa8184bb28c3cd11e95f1cb90
NiniolaX <loveafinni16@gmail.com>
2024-05-08 15:49:59 +0100

    Fixed betty errors

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -31,13 +31,13 @@ int jump_search(int *array, size_t size, int value)
 		if (value > array[right])
 		{
 			left = right;
-			right = right + jump_step;
+			right += jump_step;
 			continue;
 		}
 
 		printf("Value found between indexes [%d] and [%d]\n",
 			left, right);
-		block_size = (size_t)(right - left + 1);
+		block_size = right - left + 1;
 		return (linear_search(array + left, block_size, value));
 	}
 	return (-1);
@@ -50,7 +50,7 @@ int jump_search(int *array, size_t size, int value)
  * @array: Pointer to first element of array to search
  * @size: Size of array
  * @value: Value to search for
- * 
+ *
  * Return: First index where value is located or -1 if value is not found.
  */
 int linear_search(int *array, size_t size, int value)
