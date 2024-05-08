cdeb023e1e4b89aa71d6fcf88c1f4ce3d2ffb7ec
NiniolaX <loveafinni16@gmail.com>
2024-05-08 15:06:18 +0100

    Added linear function to file

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -42,3 +42,33 @@ int jump_search(int *array, size_t size, int value)
 	}
 	return (-1);
 }
+
+
+/**
+ * linear_search - Searches for a value in an array of integes using Linear
+ * Search Algorithm.
+ * @array: Pointer to first element of array to search
+ * @size: Size of array
+ * @value: Value to search for
+ * 
+ * Return: First index where value is located or -1 if value is not found.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	/* Return -1 if array is NULL */
+	if (array == NULL || size == 0)
+		return (-1);
+
+	/* Progressively search each element of array */
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+
+	/* Return -1 if value is not found in array */
+	return (-1);
+}
