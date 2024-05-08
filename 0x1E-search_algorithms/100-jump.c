706398b1ce6bc047442260ca68e282ae228d1636
NiniolaX <loveafinni16@gmail.com>
2024-05-08 21:08:22 +0100

    Fixed logical error in code

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -15,60 +15,38 @@ int linear_search(int *array, size_t size, int value);
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int jump_step = sqrt(size);
+	int jump_step = sqrt(size), i;
 	int left = 0, right = left + jump_step;
-	size_t block_size;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
 	while (right < (int)size)
 	{
+		/* Compare value to element at left */
 		printf("Value checked array[%d] = [%d]\n", left, array[left]);
 		if (value == array[left])
 			return (left);
 
-		if (value > array[right])
+		/* Jump to next block until block containing value is found */
+		if (value > array[left] && value > array[right])
 		{
 			left = right;
 			right += jump_step;
 			continue;
 		}
-
-		printf("Value found between indexes [%d] and [%d]\n",
-			left, right);
-		block_size = right - left + 1;
-		return (linear_search(array + left, block_size, value));
+		else
+			break;
 	}
-	return (-1);
-}
 
-
-/**
- * linear_search - Searches for a value in an array of integes using Linear
- * Search Algorithm.
- * @array: Pointer to first element of array to search
- * @size: Size of array
- * @value: Value to search for
- *
- * Return: First index where value is located or -1 if value is not found.
- */
-int linear_search(int *array, size_t size, int value)
-{
-	int i;
-
-	/* Return -1 if array is NULL */
-	if (array == NULL || size == 0)
-		return (-1);
-
-	/* Progressively search each element of array */
-	for (i = 0; i < (int)size; i++)
+	/* Linear search subarray block */
+	printf("Value found between indexes [%d] and [%d]\n", left, right);
+	for (i = left; i <= right && i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (value == array[i])
 			return (i);
 	}
 
-	/* Return -1 if value is not found in array */
 	return (-1);
 }
